e7ad7243528804c26eccadf6f97098f14e27255b
Jim Kang <jimkang@gmail.com>
2019-11-13 17:17:54 -0500

    Animate gradients.

--- dom/render-linear-gradient-defs.js
+++ dom/render-linear-gradient-defs.js
@@ -1,6 +1,7 @@
 var d3 = require('d3-selection');
 var accessor = require('accessor');
 var { parseHCLColorToRGBString } = require('../linear-gradient');
+var interpolateHCL = require('../interpolate-hcl');
 
 var defs = d3.select('defs');
 
@@ -9,13 +10,20 @@ function renderLinearGradientDefs(defObjects) {
     .selectAll('linearGradient')
     .data(defObjects, accessor());
   gradientDefs.exit().remove();
-  var newGradientDefs = gradientDefs
-    .enter()
-    .append('linearGradient');
+  var newGradientDefs = gradientDefs.enter().append('linearGradient');
   newGradientDefs
     .append('stop')
     .classed('begin-stop', true)
     .attr('offset', '0%');
+  newGradientDefs
+    .append('stop')
+    .classed('middle-stop', true)
+    .attr('offset', '50%')
+    .append('animate')
+    .attr('attributeName', 'offset')
+    .attr('values', '.95;.80;.60;.40;.20;0;.20;.40;.60;.80;.95')
+    .attr('dur', '2.5s')
+    .attr('repeatCount', 'indefinite');
   newGradientDefs
     .append('stop')
     .classed('end-stop', true)
@@ -29,6 +37,7 @@ function renderLinearGradientDefs(defObjects) {
     .attr('y1', accessor('y1'))
     .attr('y2', accessor('y2'));
   activeGradientDefs.select('.begin-stop').attr('stop-color', getBeginColor);
+  activeGradientDefs.select('.middle-stop').attr('stop-color', getMiddleColor);
   activeGradientDefs.select('.end-stop').attr('stop-color', getEndColor);
 }
 
@@ -38,6 +47,10 @@ function getBeginColor({ beginColor }) {
 
 function getEndColor({ endColor }) {
   return parseHCLColorToRGBString(endColor);
+}
+
+function getMiddleColor({ beginColor, endColor }) {
+  return parseHCLColorToRGBString(interpolateHCL(beginColor, endColor)(0.5));
 }
 
 module.exports = renderLinearGradientDefs;
