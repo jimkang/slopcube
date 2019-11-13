196fbe2e6fb424fa63f463a11fc0fb7694469425
Jim Kang <jimkang@gmail.com>
2019-11-13 12:43:54 -0500

    Now working with types.

--- types.ts
+++ types.ts
@@ -4,8 +4,32 @@ export interface HSLColor {
   l: number
 }
 
-export interface Vertex {
+export interface Spot {
   pt: [number, number],
-  color: HSLColor
+  color: HSLColor,
+  contourIndex?: number
 };
+
+export interface Line {
+  id: string,
+  coords: [[number, number], [number, number]],
+  ptA: Spot,
+  ptB: Spot
+};
+
+/*
+Avoid:
+> var a = { one: [1, 0] }
+undefined
+> b = { two: a.one }
+{ two: [ 1, 0 ] }
+> a.one[1] = 2
+2
+> b
+{ two: [ 1, 2 ] }
+> 
+*/
+export function copyPt(pt):[number, number] {
+  return [pt[0], pt[1]];
+}
 
